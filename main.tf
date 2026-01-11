69e7898bc89fcad74b6eb329485c00cb4f76d97b
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:56:37 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_oracle_autonomous_database" "oracle_autonomous_databases" {
+  for_each = var.oracle_autonomous_databases
+
+  admin_password                   = each.value.admin_password
+  resource_group_name              = each.value.resource_group_name
+  national_character_set           = each.value.national_character_set
+  name                             = each.value.name
+  mtls_connection_required         = each.value.mtls_connection_required
+  location                         = each.value.location
+  license_model                    = each.value.license_model
+  db_workload                      = each.value.db_workload
+  display_name                     = each.value.display_name
+  data_storage_size_in_tbs         = each.value.data_storage_size_in_tbs
+  compute_model                    = each.value.compute_model
+  compute_count                    = each.value.compute_count
+  character_set                    = each.value.character_set
+  backup_retention_period_in_days  = each.value.backup_retention_period_in_days
+  auto_scaling_for_storage_enabled = each.value.auto_scaling_for_storage_enabled
+  auto_scaling_enabled             = each.value.auto_scaling_enabled
+  db_version                       = each.value.db_version
+  tags                             = each.value.tags
+  allowed_ips                      = each.value.allowed_ips
+  customer_contacts                = each.value.customer_contacts
+  subnet_id                        = each.value.subnet_id
+  virtual_network_id               = each.value.virtual_network_id
+
+  dynamic "long_term_backup_schedule" {
+    for_each = each.value.long_term_backup_schedule != null ? [each.value.long_term_backup_schedule] : []
+    content {
+      enabled                  = long_term_backup_schedule.value.enabled
+      repeat_cadence           = long_term_backup_schedule.value.repeat_cadence
+      retention_period_in_days = long_term_backup_schedule.value.retention_period_in_days
+      time_of_backup           = long_term_backup_schedule.value.time_of_backup
+    }
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,66 @@
+variable "oracle_autonomous_databases" {
+  description = <<EOT
+Map of oracle_autonomous_databases, attributes below
+Required:
+    - admin_password
+    - auto_scaling_enabled
+    - auto_scaling_for_storage_enabled
+    - backup_retention_period_in_days
+    - character_set
+    - compute_count
+    - compute_model
+    - data_storage_size_in_tbs
+    - db_version
+    - db_workload
+    - display_name
+    - license_model
+    - location
+    - mtls_connection_required
+    - name
+    - national_character_set
+    - resource_group_name
+Optional:
+    - allowed_ips
+    - customer_contacts
+    - subnet_id
+    - tags
+    - virtual_network_id
+    - long_term_backup_schedule (block):
+        - enabled (required)
+        - repeat_cadence (required)
+        - retention_period_in_days (required)
+        - time_of_backup (required)
+EOT
+
+  type = map(object({
+    admin_password                   = string
+    resource_group_name              = string
+    national_character_set           = string
+    name                             = string
+    mtls_connection_required         = bool
+    location                         = string
+    license_model                    = string
+    db_workload                      = string
+    display_name                     = string
+    data_storage_size_in_tbs         = number
+    compute_model                    = string
+    compute_count                    = number
+    character_set                    = string
+    backup_retention_period_in_days  = number
+    auto_scaling_for_storage_enabled = bool
+    auto_scaling_enabled             = bool
+    db_version                       = string
+    tags                             = optional(map(string))
+    allowed_ips                      = optional(set(string))
+    customer_contacts                = optional(list(string))
+    subnet_id                        = optional(string)
+    virtual_network_id               = optional(string)
+    long_term_backup_schedule = optional(object({
+      enabled                  = bool
+      repeat_cadence           = string
+      retention_period_in_days = number
+      time_of_backup           = string
+    }))
+  }))
+}
+
